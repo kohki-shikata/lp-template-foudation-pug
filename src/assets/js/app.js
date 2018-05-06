57b614ea791fa1f819095eb1efa3b116ec132952
Kohki SHIKATA <no-replay@example.com>
2018-05-06 22:15:37 +0900

    initial commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).foundation();
+
+
+// import custom dependencies
+
+import libs from './lib/dependencies';
+
+window.libs = libs;
+
+// write down here codes for custom dependencies
---
+++ src/assets/js/lib/dependencies.js
@@ -0,0 +1,11 @@
+// custom dependencies
+
+// import slick from 'slick-carousel';
+
+// const deps = {
+//   slick: slick
+// }
+//
+// module.exports = deps;
+
+module.exports = deps;
