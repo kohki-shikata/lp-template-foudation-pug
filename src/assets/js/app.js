89b4868ba717dafefe9c9c125b344a2b84a98b81
Kohki SHIKATA <no-replay@example.com>
2018-05-07 03:10:22 +0900

    add slick carousel

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,3 +20,34 @@ window.libs = libs;
 // write down here codes for custom dependencies
 
 libs.objectFitImages('img.ofi');
+
+let sw = Foundation.MediaQuery.get('medium');
+sw = sw.replace(/[^0-9^\.]/g,"") * 16;
+let mw = Foundation.MediaQuery.get('large');
+mw = mw.replace(/[^0-9^\.]/g,"") * 16;
+
+console.log(sw);
+
+$('.slick').slick({
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  centerMode: true,
+  centerPadding: '60px',
+  responsive: [
+    {
+      breakpoint: sw,
+      settings: {
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: mw,
+      settings: {
+        slidesToShow: 3
+      }
+    }
+  ]
+});
--- src/assets/js/lib/dependencies.js
+++ src/assets/js/lib/dependencies.js
@@ -1,9 +1,11 @@
 // custom dependencies
 
 import objectFitImages from 'object-fit-images';
+import slick from 'slick-carousel';
 
 const deps = {
-  objectFitImages: objectFitImages
+  objectFitImages: objectFitImages,
+  slick: slick
 }
 
 module.exports = deps;
